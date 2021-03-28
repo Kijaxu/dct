c03ba4e112ebba14066585e438387ca20e17b9f2
Jonathan Toppins <jtoppins@users.noreply.github.com>
2021-03-28 02:08:17 -0400

    ai: implement mission FSM
    Implements a FSM inside the Mission class. The defined states are:
    
     * preparing
     * active
     * complete
     * timeout
    
    These states combined with a plan of actions allow for a more generic
    mission infrastructure. The active state executes the plan and upon
    completion of the plan the mission is a success.
    
    Closes: #27

--- src/dct/assets/Player.lua
+++ src/dct/assets/Player.lua
@@ -249,6 +249,7 @@ end
 local Player = class("Player", AssetBase)
 function Player:__init(template, region)
 	AssetBase.__init(self, template, region)
+	self.inair = false
 	self._operstate = false
 	trigger.action.setUserFlag(self.name, false)
 	trigger.action.setUserFlag(build_kick_flagname(self.name),
@@ -304,6 +305,11 @@ end
 function Player:_setup()
 	self.state = EmptyState(dctenum.kickCode.SETUP)
 	self.state:enter(self)
+end
+
+function Player:inAir()
+	self.inair = self.state.inair or false
+	return self.inair
 end
 
 function Player:getObjectNames()
